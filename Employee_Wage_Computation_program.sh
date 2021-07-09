77fa868ace9ad46d1548757661f5ad7f6e720e87
pra152 <pravatdas211@gmail.com>
2021-07-09 12:43:06 +0530

    Refactor:to change into monthly wage

--- Employee_Wage_Computation_program.sh
+++ Employee_Wage_Computation_program.sh
@@ -7,31 +7,37 @@ if [ $randomnumber -eq $ispresent ]
 then
 	echo Employee is present.
 case "$caseChoice" in
-	0) echo "To calcualate Daily Employee Wage"
-		function calculateDailyEmployeeWage(){
-		read -p "Assume Wage per hour is:" isWagePerHour "rupees"
-		read -p "Assume Full Day hours is:" isFullDayHour
-		calculateEmployeeWage=$(( $isWagePerHour * isFullDayHour ))
-		echo "To calculate daily Employee wage:" $calculateEmployeeWage "rupees"
+	1)echo "To calcualate Daily Employee Wage"
 
-              }
-#	      calculateDailyEmployeeWage
+              function calculateDailyEmployeeWage(){
+		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+		isDayHour=8
+		calculateEmployeeWageForOneDay=$(( $isWagePerHour * $isDayHour ))
+		read -p "working days :" isDay
+		calculateEmployeeWageForMonth=$(( $calculateEmployeeWageForOneDay  *  $isDay ))
+		echo "To calculate employee wages for a Month :" $calculateEmployeeWageForMonth "rupees"
+		}
+
+             calculateDailyEmployeeWage
 	      ;;
-	1) echo "To calculate part Time Employee and Wage"
+	2)echo "To calculate part Time Employee and Wage"
 
 		function partTimeEmployeeAndWage(){
-		read -p "Assume Wage per hour is:" isWagePerHour "rupees"
-        	read -p "Assume part time hours is:" isFullDayHour
-		calculatePartTimeEmployeeWage=$(( $isWagePerHour * isFullDayHour ))
-        	echo "To calculate part time Employee wage:" $calculatePartTimeEmployeeWage "rupees"
-              }
-#	      partTimeEmployeeAndWage
+		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+		isDayHour=8
+		calculateEmployeeWageForOneDay=$(( $isWagePerHour * $isDayHour ))
+		read -p "TO part time working days :" isDay
+		calculateEmployeeWageForMonth=$(( $calculateEmployeeWageForOneDay * $isDay ))
+		echo "To calculate employee part time wages for a Month :" $calculateEmployeeWageForMonth "rupees"
+		}
+
+	      partTimeEmployeeAndWage
 	      ;;
 	*) echo "No choice"
 	      ;;
 esac
+
 else
 	echo Employee is Absent
 fi
-calculateDailyEmployeeWage
-partTimeEmployeeAndWage
+
