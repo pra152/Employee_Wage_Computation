0e265392e1dc66cdd6f4f08055078f9a7cd99029
pra152 <pravatdas211@gmail.com>
2021-07-09 09:14:15 +0530

    Added:Daily Employee Wage

--- Employee_Wage_Computation_program.sh
+++ Employee_Wage_Computation_program.sh
@@ -7,3 +7,11 @@ then
 else
 	echo Employee is Absent
 fi
+function calculateDailyEmployeeWage(){
+		read -p "Assume Wage per hour is:" isWagePerHour "rupees"
+		read -p "Assume Full Day hours is:" isFullDayHour
+		calculateEmployeeWage=$(( $isWagePerHour * isFullDayHour ))
+		echo "To calculate daily Employee wage:" $calculateEmployeeWage "rupees"
+
+  }
+calculateDailyEmployeeWage
