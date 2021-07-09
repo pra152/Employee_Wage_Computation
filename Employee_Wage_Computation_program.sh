785fc4daee3dde6bc6dc578f663f4be01b7f06df
pra152 <pravatdas211@gmail.com>
2021-07-09 08:55:28 +0530

    Added:Employee is present or Absent

--- Employee_Wage_Computation_program.sh
+++ Employee_Wage_Computation_program.sh
@@ -1,1 +1,9 @@
 echo Welcome to Employee Wage Computation.
+ispresent=1
+randomnumber=$((RANDOM%2))
+if [ $randomnumber -eq $ispresent ]
+then
+	echo Employee is present.
+else
+	echo Employee is Absent
+fi
